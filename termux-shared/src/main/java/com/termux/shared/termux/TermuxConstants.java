9c0d0614a0516431036df0125dac5d8e279dc309
squiddity <squiddity@users.noreply.github.com>
2026-07-08 22:23:22 +0000

    chore: rename agent build package

--- termux-shared/src/main/java/com/termux/shared/termux/TermuxConstants.java
+++ termux-shared/src/main/java/com/termux/shared/termux/TermuxConstants.java
@@ -347,9 +347,9 @@ public final class TermuxConstants {
      */
 
     /** Termux app name */
-    public static final String TERMUX_APP_NAME = "Termux"; // Default: "Termux"
+    public static final String TERMUX_APP_NAME = "Termux (agent build)"; // Default: "Termux"
     /** Termux package name */
-    public static final String TERMUX_PACKAGE_NAME = "com.termux"; // Default: "com.termux"
+    public static final String TERMUX_PACKAGE_NAME = "com.termux.agent"; // Default: "com.termux"
     /** Termux GitHub repo name */
     public static final String TERMUX_GITHUB_REPO_NAME = "termux-app"; // Default: "termux-app"
     /** Termux GitHub repo url */
